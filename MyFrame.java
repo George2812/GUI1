77725730ba5e4ba8240682b2673500c0cf4c33c0
George Dialynas <dialynas1@yahoo.com>
2020-02-13 14:11:30 +0200

    New Buttons

--- MyFrame.java
+++ MyFrame.java
@@ -1,9 +1,15 @@
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class MyFrame extends JFrame {
 
 	private JTextField inputField,outputField;
-	private JButton button;
+	private JButton button,button2;
 	private JPanel panel;
 	
 	public MyFrame() {
@@ -13,18 +19,43 @@ public class MyFrame extends JFrame {
 		inputField=new JTextField("Enter your name");
 		outputField=new JTextField(20);
 		button=new JButton("Greeting");
+		button2=new JButton("Greeting 2");
 		
 		panel.add(inputField);
 		panel.add(button);
+		panel.add(button2);
 		panel.add(outputField);
 		
 		this.setContentPane(panel);
+		//βήμα 3 - δημιουργία αντικειμένου ακροατή 
+		ButtonListener listener=new ButtonListener();
+		//Σύνδεση ακροατή και πηγής συμβάντων
+		button.addActionListener(listener);
+		button2.addActionListener(listener);
 		
 		this.setVisible(true);
 		this.setSize(400, 400);
 		this.setTitle("Event Handling Example");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		
+	}
+	
+	//βήμα 1-Δημιουργία κλάσης ακροατή
+	class ButtonListener implements ActionListener{
+		
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource()==button) {
+				
+			
+			//βήμα 2 - Συγγραφή κώδικα
+			String name=inputField.getText();
+			outputField.setText("Hello my friend: "+name);
+			}
+			else 
+				outputField.setText("What a funny name");
+			
+		}
 		
 	}
 }
